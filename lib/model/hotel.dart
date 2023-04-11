af7e03efd7fc8bb2d0f150f70319d080272ef2fb
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-04-11 10:32:49 +0900

    feat: Hotelのモデル作成

---
+++ lib/model/hotel.dart
@@ -0,0 +1,15 @@
+class Hotel {
+  final String name;
+  final String place;
+  final String price;
+  final String tagline;
+  final String imageLink;
+
+  Hotel({
+    required this.name,
+    required this.place,
+    required this.price,
+    required this.tagline,
+    required this.imageLink,
+  });
+}
