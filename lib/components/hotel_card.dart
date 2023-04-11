dd74fad84fb06a24e0a976a8ffa84ea713035e69
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-04-11 21:25:12 +0900

    feat: カードをタップしたら詳細画面に遷移するよう実装

--- lib/components/hotel_card.dart
+++ lib/components/hotel_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playgroundflutterapp/screens/hotel_detail_screen.dart';
 
 class HotelCard extends StatelessWidget {
   String imageLink;
@@ -15,74 +16,111 @@ class HotelCard extends StatelessWidget {
     required this.tagline,
   });
 
+  // goToHotelDetailScreenメソッド作成する
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(12),
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(24),
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return const HotelDetailScreen();
+          },
+        ),
       ),
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              Ink.image(
-                image: NetworkImage(imageLink),
-                height: 240,
-                fit: BoxFit.cover,
-              ),
-              const Positioned(
-                top: 8,
-                right: 8,
-                child: Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(32.0),
-                    ),
-                    onTap: null,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.favorite_border,
-                        color: Colors.white,
+      child: Card(
+        margin: const EdgeInsets.all(12),
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Ink.image(
+                  image: NetworkImage(imageLink),
+                  height: 240,
+                  fit: BoxFit.cover,
+                ),
+                const Positioned(
+                  top: 8,
+                  right: 8,
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(32.0),
+                      ),
+                      onTap: null,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.favorite_border,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16).copyWith(bottom: 0),
-            child: GestureDetector(
-              onTap: null,
-              child: Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                ),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                  child: Stack(
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 16, top: 8, bottom: 8),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16).copyWith(bottom: 0),
+              child: GestureDetector(
+                onTap: null,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 16, top: 8, bottom: 8),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          hotelName,
+                                          textAlign: TextAlign.left,
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 22,
+                                          ),
+                                        ),
+                                        Text(
+                                          place,
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.grey.withOpacity(0.8),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(right: 16, top: 8),
                                   child: Column(
                                     mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
                                     children: <Widget>[
                                       Text(
-                                        hotelName,
+                                        '¥$price〜',
                                         textAlign: TextAlign.left,
                                         style: const TextStyle(
                                           fontWeight: FontWeight.w600,
@@ -90,51 +128,27 @@ class HotelCard extends StatelessWidget {
                                         ),
                                       ),
                                       Text(
-                                        place,
+                                        tagline,
                                         style: TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.grey.withOpacity(0.8),
-                                        ),
+                                            fontSize: 14,
+                                            color:
+                                                Colors.grey.withOpacity(0.8)),
                                       ),
                                     ],
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(right: 16, top: 8),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: <Widget>[
-                                    Text(
-                                      '¥$price〜',
-                                      textAlign: TextAlign.left,
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.w600,
-                                        fontSize: 22,
-                                      ),
-                                    ),
-                                    Text(
-                                      tagline,
-                                      style: TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.grey.withOpacity(0.8)),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
