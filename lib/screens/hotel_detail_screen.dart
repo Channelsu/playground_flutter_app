61f602603bf42e27aae573609dd01704c155082d
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-04-12 18:56:39 +0900

    feat: 仮のホテル詳細画面を作成

--- lib/screens/hotel_detail_screen.dart
+++ lib/screens/hotel_detail_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:playgroundflutterapp/model/hotel.dart';
 
 class HotelDetailScreen extends StatelessWidget {
-  const HotelDetailScreen({super.key});
+  final Hotel hotel;
+  const HotelDetailScreen({
+    super.key,
+    required this.hotel,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(hotel.name),
+      ),
+      body: Ink.image(
+        image: NetworkImage(hotel.imageLink),
+        height: 240,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
