eeaaeacc5ef4fc86f00815a09b30e257bf096ebd
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-04-23 22:01:08 +0900

    feat: idとbrandを追記

--- lib/data/hotel_list.dart
+++ lib/data/hotel_list.dart
@@ -2,6 +2,8 @@ import 'package:playgroundflutterapp/model/hotel.dart';
 
 List<Hotel> hotelList = [
   Hotel(
+    id: 1,
+    brand: 'hoshinoya',
     name: '星のや軽井沢',
     place: '長野県・軽井沢',
     price: '38,115',
@@ -10,6 +12,8 @@ List<Hotel> hotelList = [
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2013/10/aa6bf396cf71ed640fd98ae8e0cf71e8.jpg',
   ),
   Hotel(
+    id: 1001,
+    brand: 'kai',
     name: '界 加賀',
     place: '石川県・山代温泉',
     price: '16,000',
@@ -18,6 +22,8 @@ List<Hotel> hotelList = [
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2018/03/6dc82d66815383e8b90e644c43410de1.jpg',
   ),
   Hotel(
+    id: 2001,
+    brand: 'omo',
     name: 'OMO7大阪',
     place: '大阪府・大阪市',
     price: '12,000',
@@ -26,6 +32,8 @@ List<Hotel> hotelList = [
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2021/10/NEW_OMOosakaKV_2880_1822.jpg',
   ),
   Hotel(
+    id: 3001,
+    brand: 'beb',
     name: 'BEB5軽井沢',
     place: '長野県・軽井沢',
     price: '7,433',
@@ -34,6 +42,8 @@ List<Hotel> hotelList = [
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2020/10/6050_1822_1E2A7984-2.jpg',
   ),
   Hotel(
+    id: 10001,
+    brand: 'other',
     name: '青森屋',
     place: '青森県・三沢',
     price: '8,195',
