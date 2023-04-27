bd32c5f3af64a3c38b8e9d4148b63069d44ae59d
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-04-27 12:20:50 +0900

    feat: 定数のファイル群を作成し、独自のマテリアルカラーを定義

---
+++ lib/constants/colors.dart
@@ -0,0 +1,19 @@
+// アプリで使用する色を定義するファイル。
+import 'package:flutter/material.dart';
+
+// アプリ全体に適用する独自のマテリアルカラーを定義
+const MaterialColor customSwatch = MaterialColor(
+  0xFF000000, // 色のコード
+  <int, Color>{
+    50: Color(0xFFE0E0E0),
+    100: Color(0xFFB3B3B3),
+    200: Color(0xFF808080),
+    300: Color(0xFF4D4D4D),
+    400: Color(0xFF262626),
+    500: Color(0xFF000000),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
