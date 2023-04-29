d7467e532a17b5679e91c5b4d5ecea5c991362ec
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-04-29 20:43:15 +0900

    feat: ホテルのデータを追加

--- lib/data/hotel_list.dart
+++ lib/data/hotel_list.dart
@@ -12,6 +12,26 @@ List<Hotel> hotelList = [
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2013/10/aa6bf396cf71ed640fd98ae8e0cf71e8.jpg',
   ),
   Hotel(
+    id: 2,
+    brand: 'hoshinoya',
+    name: '星のや東京',
+    place: '東京都・大手町',
+    price: '50,193',
+    tagline: '世界都市東京によみがえる',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2013/10/fg2205_Thoshi_02_01-31.jpg',
+  ),
+  Hotel(
+    id: 3,
+    brand: 'hoshinoya',
+    name: '星のや富士',
+    place: '山梨県・富士河口湖',
+    price: '49,995',
+    tagline: '明日が分からない、丘陵',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2018/03/9f998229562dbe1d89aae81e37ec8fa5.jpg',
+  ),
+  Hotel(
     id: 1001,
     brand: 'kai',
     name: '界 加賀',
@@ -20,6 +40,26 @@ List<Hotel> hotelList = [
     tagline: '加賀伝統の温泉宿',
     imageLink:
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2018/03/6dc82d66815383e8b90e644c43410de1.jpg',
+  ),
+  Hotel(
+    id: 1002,
+    brand: 'kai',
+    name: '界 湯布院',
+    place: '大分県・湯布院温泉',
+    price: '37,000',
+    tagline: '四季折々の棚田',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2022/04/main_yufuin_2880-1822.jpg',
+  ),
+  Hotel(
+    id: 1003,
+    brand: 'kai',
+    name: '界 仙石原',
+    place: '神奈川県・仙石原温泉',
+    price: '46,000',
+    tagline: '内なる表現欲と出会う',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2018/04/8b9f019c86281543243551f33dffb9aa.jpg',
   ),
   Hotel(
     id: 2001,
@@ -32,6 +72,26 @@ List<Hotel> hotelList = [
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2021/10/NEW_OMOosakaKV_2880_1822.jpg',
   ),
   Hotel(
+    id: 2002,
+    brand: 'omo',
+    name: 'OMO7高知',
+    place: '高知府・高知市',
+    price: '5,273',
+    tagline: '歴史と活気を感じる街',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2022/10/b5a24819a2134859ce56ef27cf846ae4.jpg',
+  ),
+  Hotel(
+    id: 2003,
+    brand: 'omo',
+    name: 'OMO5小樽',
+    place: '北海道・小樽市',
+    price: '5,455',
+    tagline: 'ソーラン、目覚めの港町',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2021/10/OMOotaruKV_2880_1822.jpg',
+  ),
+  Hotel(
     id: 3001,
     brand: 'beb',
     name: 'BEB5軽井沢',
@@ -42,6 +102,16 @@ List<Hotel> hotelList = [
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2020/10/6050_1822_1E2A7984-2.jpg',
   ),
   Hotel(
+    id: 3002,
+    brand: 'beb',
+    name: 'BEB5土浦',
+    place: '茨城県・土浦',
+    price: '5,400',
+    tagline: '自転車好きも嬉しい駅ウエ',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2019/10/Yikin-HYO-IZY_0346-top.jpg',
+  ),
+  Hotel(
     id: 10001,
     brand: 'other',
     name: '青森屋',
@@ -50,5 +120,25 @@ List<Hotel> hotelList = [
     tagline: 'ハネトが舞い、祭り気分最高',
     imageLink:
         'https://www.hoshinoresorts.com/hrcms/wp-content/images/2018/03/bb2941b3025267abc537ce36534097c0.jpg',
+  ),
+  Hotel(
+    id: 10002,
+    brand: 'other',
+    name: '奥入瀬渓流ホテル',
+    place: '青森県・十和田',
+    price: '14,864',
+    tagline: '阿修羅の流れが躍動する',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2013/10/37f245062fa21a91ab58b0be400d9251.jpg',
+  ),
+  Hotel(
+    id: 10003,
+    brand: 'other',
+    name: '西表島ホテル',
+    place: '沖縄県・西表島',
+    price: '12,364',
+    tagline: 'イリオモテヤマネコが棲む',
+    imageLink:
+        'https://www.hoshinoresorts.com/hrcms/wp-content/images/2019/08/e9a8e225bab88e46ac078ccd36ff818c.jpg',
   ),
 ];
