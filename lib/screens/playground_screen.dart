ae5b59942595886e52ddd09a63e687d36b748bc8
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-05-15 12:56:29 +0900

    feat: 2倍ボタン実装

--- lib/screens/playground_screen.dart
+++ lib/screens/playground_screen.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class PlaygroundScreen extends StatelessWidget {
+class PlaygroundScreen extends StatefulWidget {
   const PlaygroundScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PlaygroundScreen> createState() => _PlaygroundScreenState();
+}
+
+class _PlaygroundScreenState extends State<PlaygroundScreen> {
+  // 状態（state）
+  int _count = 1;
+
+  // ロジック
+  void countTwice() {
+    setState(() {
+      _count = _count * 2;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        // 上下（Rowの場合は左右）中央に寄せる
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('abc')],
+      body: Center(
+        child: Column(
+          // 上下（Rowの場合は左右）中央に寄せる
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$_count'),
+            OutlinedButton(
+              onPressed: countTwice,
+              child: const Text('2倍！！'),
+            )
+          ],
+        ),
       ),
     );
   }
