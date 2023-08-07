0d5eef33a9bf92730ef15d3e8ea24113050eccc9
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-07 22:27:01 +0900

    feat: firebaseのgetお試し

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,12 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
+  Future<String> getDoc() async {
+    DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
+        .instance
+        .collection('users')
+        .doc('K0URgtawDiN3C2Q9tfFx')
+        .get();
+    return snapshot.data()!['name'];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('設定画面', style: TextStyle(fontSize: 32.0))),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color: Colors.black12,
+            height: 16,
+          ),
+          Container(
+            color: Colors.blueAccent,
+            width: double.infinity,
+            child: FutureBuilder(
+              future: getDoc(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data);
+                } else {
+                  return const Text('データなし');
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
