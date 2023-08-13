7f5e001f56be7e8acbd6e7bcc10c94183c39440a
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-13 19:41:13 +0900

    feat: 一覧画面作成

--- lib/screens/basic_widgets_screen.dart
+++ lib/screens/basic_widgets_screen.dart
@@ -5,8 +5,65 @@ class BasicWidgetsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('基本'),
+    return Scaffold(
+      body: GridView.count(
+        padding: const EdgeInsets.all(8.0),
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        childAspectRatio: 2 / 2.25, // Todo:ここでCardの高さ調整
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        children: [
+          for (var i = 0; i < 7; i++)
+            Card(
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+              child: Column(
+                children: [
+                  // ウィジェット画像
+                  SizedBox(
+                    width: double.infinity,
+                    height: 128,
+                    child: Image.network(
+                      'https://source.unsplash.com/100x60/?shoe',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  // ウィジェット名
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Text(
+                      'AppBar',
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                  // ウィジェット説明文
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Text(
+                      '説明文説明文説明文説明文説明文',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          // for (final likeGoods in likeList)
+          //   GoodsCard(
+          //     goods: likeGoods,
+          //     isLiked: true,
+          //     showLikeCount: true,
+          //   ) // ループ書き方2
+          // ...likeList
+          //     .map((likeGoods) => GoodsCard(goods: likeGoods, isLiked: true, showLikeCount: true))
+          //     .toList() // ループ書き方3
+        ],
+      ),
     );
   }
 }
