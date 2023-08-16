9f548c0b0cce3e1ca4681d2220bd2b0c385f1a6a
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-17 00:20:07 +0900

    chore: 不要な部分削除

--- lib/screens/english_words_screen.dart
+++ lib/screens/english_words_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:playgroundflutterapp/services/english_words_service.dart';
@@ -78,9 +77,6 @@ class EnglishWordsScreen extends HookWidget {
   Widget build(BuildContext context) {
     final englishWordController = useTextEditingController();
     final meaningController = useTextEditingController();
-    final items = List<String>.generate(20, (i) => "Item $i");
-    final englishWords =
-        FirebaseFirestore.instance.collection('english-words').get();
 
     return Scaffold(
       appBar: AppBar(
