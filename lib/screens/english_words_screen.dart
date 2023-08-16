7974ca9cf561d47a390b9ec4400cf0f1f806b2f0
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-16 12:09:14 +0900

    feat: 仮のリストビュー作成

--- lib/screens/english_words_screen.dart
+++ lib/screens/english_words_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:playgroundflutterapp/services/english_words_service.dart';
@@ -9,6 +10,9 @@ class EnglishWordsScreen extends HookWidget {
   Widget build(BuildContext context) {
     final englishWordController = useTextEditingController();
     final meaningController = useTextEditingController();
+    final items = List<String>.generate(20, (i) => "Item $i");
+    final englishWords =
+        FirebaseFirestore.instance.collection('english-words').get();
 
     return Scaffold(
       appBar: AppBar(
@@ -33,6 +37,14 @@ class EnglishWordsScreen extends HookWidget {
             controller: meaningController,
             decoration: const InputDecoration(
               hintText: '意味',
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(title: Text(items[index]));
+              },
             ),
           ),
         ],
