e4f5160bb728fa96fbf279ca05184e99668a8f1b
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-17 00:17:54 +0900

    feat: 英単語モデル作成

---
+++ lib/model/english_word.dart
@@ -0,0 +1,11 @@
+class EnglishWord {
+  String id;
+  final String title;
+  final String japanese;
+
+  EnglishWord({
+    this.id = '',
+    required this.title,
+    required this.japanese,
+  });
+}
