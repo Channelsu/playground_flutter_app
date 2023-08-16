d9ef3868543782ad32262d3c71e7f0d26281e4f8
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-16 12:22:47 +0900

    feat: ダイアログが表示されるよう実装

--- lib/screens/english_words_screen.dart
+++ lib/screens/english_words_screen.dart
@@ -1,10 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:playgroundflutterapp/services/english_words_service.dart';
 
 class EnglishWordsScreen extends HookWidget {
   const EnglishWordsScreen({super.key});
+
+  Future _showFormDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) {
+        return AlertDialog(
+          title: const Text("This is the title"),
+          content: const Text("This is the content"),
+          actions: [
+            ElevatedButton(
+              child: const Text("Cancel"),
+              onPressed: () => Navigator.pop(context),
+            ),
+            ElevatedButton(
+              child: const Text("OK"),
+              onPressed: () => print('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,18 +72,19 @@ class EnglishWordsScreen extends HookWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          final service = EnglishWordsService();
-          final document = {
-            'title': englishWordController.text,
-            'japanese': meaningController.text,
-          };
-          service.create(document);
-          debugPrint(
-              '英単語：${englishWordController.text}　意味：${meaningController.text}');
-          englishWordController.clear();
-          meaningController.clear();
-        },
+        // onPressed: () {
+        //   final service = EnglishWordsService();
+        //   final document = {
+        //     'title': englishWordController.text,
+        //     'japanese': meaningController.text,
+        //   };
+        //   service.create(document);
+        //   debugPrint(
+        //       '英単語：${englishWordController.text}　意味：${meaningController.text}');
+        //   englishWordController.clear();
+        //   meaningController.clear();
+        // },
+        onPressed: () async => await _showFormDialog(context),
         tooltip: '新規追加',
         child: const Icon(Icons.add),
       ),
