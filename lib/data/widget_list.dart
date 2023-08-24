4af5f50cbab450909e7c81a31ce4d7048812936d
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-24 16:36:59 +0900

    feat: ウィジェットの配列作成

---
+++ lib/data/widget_list.dart
@@ -0,0 +1,72 @@
+import 'package:playgroundflutterapp/model/wgt.dart';
+
+List<Wgt> widgetList = [
+  Wgt(
+    id: '1',
+    name: 'AppBar',
+    overview:
+        'TabBarウィジェットやFlexibleSpaceBarウィジェットのような他のウィジェットを収容することもできるツールバーになります。',
+    imagePath: 'app_bar',
+  ),
+  Wgt(
+    id: '2',
+    name: 'Column',
+    overview: '子ウィジェットのリストを垂直方向に配置するウィジェットです。',
+    imagePath: 'column',
+  ),
+  Wgt(
+    id: '3',
+    name: 'Container',
+    overview: 'カラーリングや配置、サイズ調整のウィジェットを兼ね備えた便利なウィジェットになります。',
+    imagePath: 'container',
+  ),
+  Wgt(
+    id: '4',
+    name: 'ElevatedButton',
+    overview: 'マテリアルデザインの昇降ボタン。押されると上昇する塗りつぶされたボタンです。',
+    imagePath: 'elevated_button',
+  ),
+  Wgt(
+    id: '5',
+    name: 'FlutterLogo',
+    overview: 'ウィジェット形式のFlutterのロゴになります。このウィジェットはIconThemeを順守しています。',
+    imagePath: 'flutter_logo',
+  ),
+  Wgt(
+    id: '6',
+    name: 'Icon',
+    overview: 'マテリアルデザインのアイコンを表示するウィジェットです。',
+    imagePath: 'icon',
+  ),
+  Wgt(
+    id: '7',
+    name: 'Image',
+    overview: '画像を表示するウィジェットです。',
+    imagePath: 'image',
+  ),
+  Wgt(
+    id: '8',
+    name: 'Placeholder',
+    overview: '今後ウィジェットが追加される場所を表すためのボックスを描くウィジェットです。',
+    imagePath: 'flutter_logo',
+  ),
+  Wgt(
+    id: '9',
+    name: 'Row',
+    overview: '子ウィジェットのリストを水平方向に配置するウィジェットです。',
+    imagePath: 'row',
+  ),
+  Wgt(
+    id: '10',
+    name: 'Scaffold',
+    overview:
+        'マテリアルデザインの基本的なビジュアルレイアウト構造を実装します。このクラスは、ドロワー、スナックバー、ボトムシートを表示するためのAPIを提供します。',
+    imagePath: 'scaffold',
+  ),
+  Wgt(
+    id: '11',
+    name: 'Text',
+    overview: '一行での文章を表示するウィジェットです。',
+    imagePath: 'text',
+  ),
+];
