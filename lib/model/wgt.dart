415b1a92c8a8251a69f789aa7287034e88e5d49a
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-08-24 14:43:31 +0900

    feat: ウィジェットモデル作成

---
+++ lib/model/wgt.dart
@@ -0,0 +1,13 @@
+class Wgt {
+  final String id;
+  final String name;
+  final String overview;
+  final String imagePath;
+
+  Wgt({
+    required this.id,
+    required this.name,
+    required this.overview,
+    required this.imagePath,
+  });
+}
