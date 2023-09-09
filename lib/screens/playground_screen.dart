8a67d4e6256f2849acf29506a92ef6cbf26a90ae
Channelsu <40008019+Channelsu@users.noreply.github.com>
2023-09-10 07:50:52 +0900

    feat: 縦長カードサンプル2作成

--- lib/screens/playground_screen.dart
+++ lib/screens/playground_screen.dart
@@ -488,6 +488,57 @@ class _PlaygroundScreenState extends State<PlaygroundScreen> {
                 ),
               ),
               const SizedBox(height: 12),
+              // 縦長カードサンプル2
+              Container(
+                width: MediaQuery.of(context).size.width * 0.4,
+                margin: const EdgeInsets.all(12),
+                padding: const EdgeInsets.only(top: 30, bottom: 30),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 10,
+                      offset: Offset(-10, -10),
+                      color: Colors.white24,
+                    ),
+                    BoxShadow(
+                      blurRadius: 10,
+                      offset: Offset(10, 10),
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Column(
+                    children: [
+                      // アイコン
+                      const Icon(
+                        Icons.rocket_launch,
+                        color: Colors.black45,
+                        size: 48,
+                      ),
+                      const SizedBox(height: 8),
+                      // タイトル
+                      Text(
+                        'タイトル',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 8),
+                      // 説明文
+                      Text(
+                        'ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12),
             ],
           ),
         ),
